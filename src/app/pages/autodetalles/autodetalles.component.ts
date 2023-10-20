ec76a7c8f52e4c59d858fd08caa429e3bf8ac78d
francisco peñaloza <fpa199725@gmail.com>
2023-10-20 14:02:36 -0600

    detalles mejoras 3

--- src/app/pages/autodetalles/autodetalles.component.ts
+++ src/app/pages/autodetalles/autodetalles.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/service/api.service';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-autodetalles',
@@ -8,7 +9,7 @@ import { ApiService } from 'src/app/service/api.service';
   styleUrls: ['./autodetalles.component.css']
 })
 export class AutodetallesComponent implements  OnInit {
-  constructor(private apiService:ApiService,private route: ActivatedRoute){}
+  constructor(private apiService:ApiService,private route: ActivatedRoute,private meta: Meta){}
   auto:any;
   autosimilares:any[]=[];
   autoId: any;
@@ -17,6 +18,11 @@ export class AutodetallesComponent implements  OnInit {
 
     this.apiService.obtenerAutoPorId(this.autoId).subscribe(data => {
          this.auto=data
+
+         this.meta.addTag({ property: 'og:title', content: 'Autos Cadereyta' });
+         this.meta.addTag({ property: 'og:description', content: 'autos' });
+         this.meta.addTag({ property: 'og:image', content: 'https://autoscadereytaapi.blob.core.windows.net/autos/8d1b3025-eff9-4e38-bea7-e5f4cd8cafe3.jpg' });
+         this.meta.addTag({ property: 'og:url', content: 'https://autoscadereytaapi.blob.core.windows.net/autos/8d1b3025-eff9-4e38-bea7-e5f4cd8cafe3.jpg' });
       console.log("Auto obtenido:", data);
       // Aquí puedes manejar los datos del auto obtenido
     });
